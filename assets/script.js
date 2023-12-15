d2aa7d347b28be7f59ec31cd57a95a330650b4cb
Polly <147508946+porlowska@users.noreply.github.com>
2023-12-15 16:29:38 +0000

    added comments and pseudocode

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+//url variable
+//API for daily and weekly forcas
+
+
+
+/*
+Search bar - autofillig of search input 
+Search  input should add a city to the  url which will update 
+upon xlinxking search it should add the city to the hostory and update the view on the websote for that city
+*/
+
+/* Update search history - on click a city should be added to to the search history 
+use of local storage 
+add button clear search history
+when any city pressed in search histry the forcast should display fo them  */
+
+/*
+today forcast a div that contains todays weather: 
+The city name
+The date
+An icon representation of weather conditions
+The temperature
+The humidity
+The wind speed
+*/
+
+/* for 5 days weather 5 tiles/cards
+The date
+An icon representation of weather conditions
+The temperature
+The humidity
+ */
